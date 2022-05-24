00bebc0b6e6768aeccfeb5d47d77b48b4ee6de3a
huskyhehe <zhou.he1@northeastern.edu>
2022-05-23 19:39:58 -0700

    Add User entity model class

---
+++ online-store-backend/src/main/java/com/huskyhehe/onlinestorebackend/model/Role.java
@@ -0,0 +1,6 @@
+package com.huskyhehe.onlinestorebackend.model;
+
+public enum Role {
+    USER,
+    ADMIN;
+}
--- online-store-backend/src/main/java/com/huskyhehe/onlinestorebackend/model/User.java
+++ online-store-backend/src/main/java/com/huskyhehe/onlinestorebackend/model/User.java
@@ -1,2 +1,32 @@
-package com.huskyhehe.onlinestorebackend.model;public class User {
+package com.huskyhehe.onlinestorebackend.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "username", unique = true, nullable = false, length = 100)
+    private String username;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "create_time", nullable = false)
+    private LocalDateTime createTime;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role", nullable = false)
+    private Role role;
 }
