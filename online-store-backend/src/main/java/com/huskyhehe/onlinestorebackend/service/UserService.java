c351af87e5f1911b7a13552ffe30d3e97ca8b643
huskyhehe <zhou.he1@northeastern.edu>
2022-05-24 18:05:06 -0700

    Add User service

--- online-store-backend/src/main/java/com/huskyhehe/onlinestorebackend/service/UserService.java
+++ online-store-backend/src/main/java/com/huskyhehe/onlinestorebackend/service/UserService.java
@@ -1,4 +1,14 @@
 package com.huskyhehe.onlinestorebackend.service;
 
+import com.huskyhehe.onlinestorebackend.model.Role;
+import com.huskyhehe.onlinestorebackend.model.User;
+
+import java.util.Optional;
+
 public interface UserService {
+
+    User saveUser(User user);
+    Optional<User> findByUsername(String username);
+    void changeRole(String username, Role newRole);
+
 }
--- online-store-backend/src/main/java/com/huskyhehe/onlinestorebackend/service/UserServiceImpl.java
+++ online-store-backend/src/main/java/com/huskyhehe/onlinestorebackend/service/UserServiceImpl.java
@@ -6,7 +6,9 @@ import com.huskyhehe.onlinestorebackend.repository.UserRepository;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -19,13 +21,28 @@ public class UserServiceImpl implements UserService {
         this.passwordEncoder = passwordEncoder;
     }
 
+
+    @Override
     public User saveUser(User user) {
 
-//        user.setPassword(passwordEncoder.encode(user.getPassword()));
-//        user.setRole(Role.USER);
-//        user.setCreateTime(LocalDateTime.now());
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.setRole(Role.USER);
+        user.setCreateTime(LocalDateTime.now());
 
         return userRepository.save(user);
+    }
+
+
+    @Override
+    public Optional<User> findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+
+
+    @Override
+    @Transactional // transactional is required when executing an update/delete query.
+    public void changeRole(String username, Role newRole) {
+        userRepository.updateUserRole(username, newRole);
     }
 
 }
