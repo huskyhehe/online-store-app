1141b90b4b9d3bcbd78b2e3c755e03f8f5e1c3b3
huskyhehe <zhou.he1@northeastern.edu>
2022-05-31 21:20:22 -0700

    Update security config

--- online-store-backend/src/main/java/com/huskyhehe/onlinestorebackend/security/SecurityConfig.java
+++ online-store-backend/src/main/java/com/huskyhehe/onlinestorebackend/security/SecurityConfig.java
@@ -2,8 +2,6 @@ package com.huskyhehe.onlinestorebackend.security;
 
 import com.huskyhehe.onlinestorebackend.model.Role;
 import com.huskyhehe.onlinestorebackend.security.jwt.JwtAuthorizationFilter;
-import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,46 +9,34 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private CustomUserDetailsService customUserDetailsService;
 
-
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception
-    {
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(customUserDetailsService).passwordEncoder(passwordEncoder());
     }
 
-
-//    @Bean(BeanIds.AUTHENTICATION_MANAGER)
-//    public AuthenticationManager authenticationManagerBean() throws Exception
-//    {
-//        return super.authenticationManagerBean();
-//    }
-
-    @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
-        return authenticationConfiguration.getAuthenticationManager();
+    @Bean(BeanIds.AUTHENTICATION_MANAGER)
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 
-
+    @Override
     protected void configure(HttpSecurity http) throws Exception
     {
         http.cors();
@@ -95,16 +81,7 @@ public class SecurityConfig {
     }
 }
 
-//
-//@Configuration
-//@EnableWebSecurity
-//@RequiredArgsConstructor
-//@Component
-////@ImportResource({"classpath:SecurityConfig.xml"})
-//public class SecurityConfig {
-//
-//    @Autowired
-//    private CustomUserDetailsService customUserDetailsService;
+
 //
 //    @Bean
 //    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
@@ -127,26 +104,3 @@ public class SecurityConfig {
 //
 //        return http.build();
 //    }
-//
-//    @Bean
-//    public JwtAuthorizationFilter jwtAuthorizationFilter() {
-//        return new JwtAuthorizationFilter();
-//    }
-//
-//    @Bean
-//    public PasswordEncoder passwordEncoder() {
-//        return new BCryptPasswordEncoder();
-//    }
-//
-//    @Bean
-//    public WebMvcConfigurer corsConfigurer() {
-//        return new WebMvcConfigurer() {
-//            @Override
-//            public void addCorsMappings(CorsRegistry registry) {
-//                registry.addMapping("/**")
-//                        .allowedOrigins("*")
-//                        .allowedMethods("*");
-//            }
-//        };
-//    }
-//}
