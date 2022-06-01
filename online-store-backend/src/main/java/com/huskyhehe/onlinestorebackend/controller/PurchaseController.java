9874fb7b086663a5744607d6ce78997ea0ae2fa3
huskyhehe <zhou.he1@northeastern.edu>
2022-05-31 21:19:21 -0700

    Add purchase controller

--- online-store-backend/src/main/java/com/huskyhehe/onlinestorebackend/controller/PurchaseController.java
+++ online-store-backend/src/main/java/com/huskyhehe/onlinestorebackend/controller/PurchaseController.java
@@ -1,2 +1,30 @@
-package com.huskyhehe.onlinestorebackend.controller;public class PurchaseCotroller {
+package com.huskyhehe.onlinestorebackend.controller;
+
+import com.huskyhehe.onlinestorebackend.model.Purchase;
+import com.huskyhehe.onlinestorebackend.security.UserPrinciple;
+import com.huskyhehe.onlinestorebackend.service.PurchaseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/purchase")     // pre-path
+public class PurchaseController {
+
+    @Autowired
+    private PurchaseService purchaseService;
+
+    @PostMapping        // api/purchase
+    public ResponseEntity<?> savePurchase(@RequestBody Purchase purchase) {
+        return new ResponseEntity<>(purchaseService.savePurchase(purchase), HttpStatus.CREATED);
+    }
+
+    public ResponseEntity<?> getAllPurchasesOfUser(@AuthenticationPrincipal UserPrinciple userPrinciple) {
+        return ResponseEntity.ok(purchaseService.findPurchaseItemsOfUser(userPrinciple.getId()));
+    }
 }
