3db940cc41b02c17f597466bab9141d6ddb5aac5
huskyhehe <zhou.he1@northeastern.edu>
2022-05-31 21:19:50 -0700

    Add user controller

--- online-store-backend/src/main/java/com/huskyhehe/onlinestorebackend/controller/UserController.java
+++ online-store-backend/src/main/java/com/huskyhehe/onlinestorebackend/controller/UserController.java
@@ -1,2 +1,27 @@
-package com.huskyhehe.onlinestorebackend.controller;public class UserController {
+package com.huskyhehe.onlinestorebackend.controller;
+
+import com.huskyhehe.onlinestorebackend.model.Role;
+import com.huskyhehe.onlinestorebackend.security.UserPrinciple;
+import com.huskyhehe.onlinestorebackend.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/user")  // pre-path
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PutMapping("change/{role}")      // api/user/change/{role}
+    public ResponseEntity<?> changeRole(@AuthenticationPrincipal UserPrinciple userPrinciple, @PathVariable Role role) {
+        userService.changeRole(userPrinciple.getUsername(), role);
+        return ResponseEntity.ok(true);
+    }
+
 }
