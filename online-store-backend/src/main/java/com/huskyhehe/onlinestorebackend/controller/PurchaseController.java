b260e914375203e77028188428480ef57e7caa08
huskyhehe <zhou.he1@northeastern.edu>
2022-06-01 18:50:24 -0700

    Add purchase controller

--- online-store-backend/src/main/java/com/huskyhehe/onlinestorebackend/controller/PurchaseController.java
+++ online-store-backend/src/main/java/com/huskyhehe/onlinestorebackend/controller/PurchaseController.java
@@ -7,23 +7,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("api/purchase")     // pre-path
+@RequestMapping("api/purchase")     //pre-path
 public class PurchaseController {
 
     @Autowired
     private PurchaseService purchaseService;
 
-    @PostMapping        // api/purchase
+    @PostMapping //api/purchase
     public ResponseEntity<?> savePurchase(@RequestBody Purchase purchase) {
         return new ResponseEntity<>(purchaseService.savePurchase(purchase), HttpStatus.CREATED);
     }
 
+    @GetMapping //api/purchase
     public ResponseEntity<?> getAllPurchasesOfUser(@AuthenticationPrincipal UserPrinciple userPrinciple) {
         return ResponseEntity.ok(purchaseService.findPurchaseItemsOfUser(userPrinciple.getId()));
     }
