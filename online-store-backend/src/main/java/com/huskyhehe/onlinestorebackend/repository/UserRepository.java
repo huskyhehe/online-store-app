0d9a2d4dd318909936946d32f72360e77a44be7f
huskyhehe <zhou.he1@northeastern.edu>
2022-06-01 18:49:28 -0700

    Add user repository

--- online-store-backend/src/main/java/com/huskyhehe/onlinestorebackend/repository/UserRepository.java
+++ online-store-backend/src/main/java/com/huskyhehe/onlinestorebackend/repository/UserRepository.java
@@ -17,4 +17,5 @@ public interface UserRepository extends JpaRepository<User, Long> {
     @Modifying
     @Query("update User set role = :role where username = :username")
     void updateUserRole(@Param("username") String username, @Param("role") Role role);
+
 }
