43a41715d6280bea3c54f9545634c3ab337321db
huskyhehe <zhou.he1@northeastern.edu>
2022-06-01 18:52:49 -0700

    Update security config

--- online-store-backend/src/main/java/com/huskyhehe/onlinestorebackend/security/SecurityConfig.java
+++ online-store-backend/src/main/java/com/huskyhehe/onlinestorebackend/security/SecurityConfig.java
@@ -31,14 +31,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         auth.userDetailsService(customUserDetailsService).passwordEncoder(passwordEncoder());
     }
 
+    @Override
     @Bean(BeanIds.AUTHENTICATION_MANAGER)
     public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean();
     }
 
     @Override
-    protected void configure(HttpSecurity http) throws Exception
-    {
+    protected void configure(HttpSecurity http) throws Exception {
         http.cors();
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
@@ -54,53 +54,25 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     //Why don't we describe it as a component, because of scope
     @Bean
-    public JwtAuthorizationFilter jwtAuthorizationFilter()
-    {
+    public JwtAuthorizationFilter jwtAuthorizationFilter() {
         return new JwtAuthorizationFilter();
     }
 
     @Bean
-    public PasswordEncoder passwordEncoder()
-    {
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
     @Bean
-    public WebMvcConfigurer corsConfigurer()
-    {
-        return new WebMvcConfigurer()
-        {
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
             @Override
-            public void addCorsMappings(CorsRegistry registry)
-            {
+            public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins("*")
                         .allowedMethods("*");
             }
         };
     }
+
 }
-
-
-//
-//    @Bean
-//    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
-//        return authenticationConfiguration.getAuthenticationManager();
-//    }
-//
-//    @Bean
-//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//        http.cors();
-//        http.csrf().disable();
-//        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-//
-//        http.authorizeRequests()
-//                .antMatchers("/api/authentication/**").permitAll()
-//                .antMatchers(HttpMethod.GET, "/api/product").permitAll()
-//                .antMatchers("/api/product/**").hasRole(Role.ADMIN.name())
-//                .anyRequest().authenticated();
-//
-//        http.addFilterBefore(jwtAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
-//
-//        return http.build();
-//    }
